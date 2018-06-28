0f2432e862039597ab5bb210f6eb571713c932dc
0888946595 <namnguyenktxd@gmail.com>
2018-06-28 15:03:28 +0700

    initial commit

--- Session2/homework/dem3.py
+++ Session2/homework/dem3.py
@@ -1,3 +1,13 @@
 for i in range (5):
     i = (0, 1)
-    print (i, end="")
+    print (i, end="")       
+
+
+
+
+
+
+
+
+
+    
--- Session2/homework/tongn.py
+++ Session2/homework/tongn.py
@@ -1,11 +1,15 @@
-numb = int(input("Hay nhap so ban muon"))
-def factorial(numb):
-    if numb == 0:
-        return 1
-    else:
-        return numb*factorial(numb-1)
+# numb = int(input("Hay nhap so ban muon: "))
+# def factorial(numb):
+#     if numb == 0:
+#         return 1
+#     else:
+#         return numb**(numb-1)
 
-print (factorial(10))
+# print (factorial(numb))
 
+from math import *
 
-        
+n = int(input("enter a number: "))
+
+result = n**n
+print (result)
