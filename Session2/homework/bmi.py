2eb9cc73b4af0d85e5563fad2cfdb9cd7bfd2298
0888946595 <namnguyenktxd@gmail.com>
2018-06-29 11:33:35 +0700

    initial commit

--- Session2/homework/bmi.py
+++ Session2/homework/bmi.py
@@ -1,8 +1,11 @@
-height = int(input("chieu cao cua ban la:"))
-weight = int(input("can nang cua ban la:"))
 
-bmi = weight/(height*height)
 
+height = int(input("chieu cao cua ban la: "))
+weight = int(input("can nang cua ban la: "))
+
+bmi = weight/((height*height)*(10**(-4)))
+
+print ("BMI = ", bmi)
 from random import randint
 
 bmi = randint(16, 30)
@@ -10,13 +13,13 @@ bmi = randint(16, 30)
 if bmi <16:
     print ("")
 
-elif bmi >16 and bmi <18.5:
+elif bmi <18.5:
     print ("ban dang bi suy dinh duong")
 
-elif bmi >18.5 and bmi <25:
+elif bmi <25:
     print ("ban rat binh thuong")
 
-elif bmi >25 and bmi <30:
+elif bmi <30:
     print ("co ve ban hoi mum mim roi")
 
 else:
