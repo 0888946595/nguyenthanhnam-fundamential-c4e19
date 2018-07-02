c33d96d72326302cba444cb5e83bf39eafb4377b
0888946595 <namnguyenktxd@gmail.com>
2018-07-02 14:32:47 +0700

    initial commit

--- Session 3/homework/turtle_2.py
+++ Session 3/homework/turtle_2.py
@@ -4,9 +4,19 @@ shape ("turtle")
 
 speed (-1)
 
-color = ["red", "blue", "bwron", "yellow", "green"]
+color = ["red", "blue", "brown", "yellow", "green"]
 
-i = 0
+i = 3
 
-for i in range (0, 4):
+for i in range (3, 7):
+
+    for j in range (i):
+        fillcolor (color[i-3])
+        begin_fill()
+        forward (100)
+        left (360/i)
+        end_fill()
     
+    
+
+mainloop()
